23a33b36f3c45a84fefeb50fcd5b49976435c508
Sankar <gsankarsvga@gmail.com>
2021-10-14 07:42:45 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Helmet } from "react-helmet";
+
+import { Nav, Navbar, Container } from "react-bootstrap";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>My app | Sankar</title>
+        </Helmet>
+
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Nav className="me-auto">
+              <Navbar.Brand href="#home">Home</Navbar.Brand>
+              <Nav.Link href="#register">Register</Nav.Link>
+
+              <Nav.Link href="#features">Login</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        <h2>Home Page</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
